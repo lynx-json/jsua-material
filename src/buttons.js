e76ce1bd65f04c22fb0453c01a856168e92cae6d
John Howes <john.howes@gmail.com>
2017-03-07 11:06:13 -0600

    Work on Expansion Panel

--- src/buttons.js
+++ src/buttons.js
@@ -1,4 +1,3 @@
-import { wrapChildren } from "./util";
 import * as text from "./text";
 import { query, on } from "jsua-query";
 import elevation from "./elevation";
@@ -33,25 +32,34 @@ export function flat(color) {
 
 export function raised(backgroundColor) {
   return [
-    shadow([
-      el => el.style.display = "flex",
-      el => el.style.flexDirection = "row",
-      el => el.style.alignItems = "center",
-      el => el.style.minHeight = "36px",
-      el => el.style.minWidth = "88px",
-      el => el.style.paddingLeft = "16px",
-      el => el.style.paddingRight = "16px",
-      el => el.style.borderRadius = "2px",
-      el => el.style.backgroundColor = backgroundColor,
-      text.button(backgroundColor),
-      elevation(2)
-    ]),
-    el => el.style.paddingTop = "6px",
-    el => el.style.paddingBottom = "6px",
-    el => el.style.cursor = "pointer",
-    on("mousedown", el => query(el.shadowRoot.firstElementChild).each(elevation(8))),
-    on("mouseup", el => query(el.shadowRoot.firstElementChild).each(elevation(2))),
-    on("touchstart", el => query(el.shadowRoot.firstElementChild).each(elevation(8))),
-    on("touchend", el => query(el.shadowRoot.firstElementChild).each(elevation(2)))
+    el => el.style.display = "flex",
+    el => el.style.flexDirection = "row",
+    el => el.style.alignItems = "center",
+    el => el.style.minHeight = "36px",
+    el => el.style.minWidth = "88px",
+    el => el.style.paddingLeft = "16px",
+    el => el.style.paddingRight = "16px",
+    el => el.style.borderRadius = "2px",
+    el => el.style.backgroundColor = backgroundColor,
+    text.button(backgroundColor),
+    elevation(2),
+    // Create a touch target
+    el => {
+      var wrapper = document.createElement("div");
+      wrapper.role = "presentation";
+      el.parentElement.replaceChild(wrapper, el);
+      wrapper.addEventListener("click", function () {
+        el.click();
+      });
+      wrapper.appendChild(el);
+      query(wrapper).each([
+        el => el.style.paddingTop = "6px",
+        el => el.style.paddingBottom = "6px",
+        on("mousedown", el => query(el.firstElementChild).each(elevation(8))),
+        on("mouseup", el => query(el.firstElementChild).each(elevation(2))),
+        on("touchstart", el => query(el.firstElementChild).each(elevation(8))),
+        on("touchend", el => query(el.firstElementChild).each(elevation(2)))
+      ]);
+    }
   ];
 }
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import responsiveUI from "./responsive-ui";
 import * as buttons from "./buttons";
 import { appBar } from "./app-bar";
 import { card } from "./card";
+import { expansionPanel } from "./expansion-panel";
 
 function backgroundColor(color) {
   return el => el.style.backgroundColor = color;
@@ -20,5 +21,6 @@ export {
   responsiveUI,
   buttons,
   appBar,
-  card
+  card,
+  expansionPanel
 };
