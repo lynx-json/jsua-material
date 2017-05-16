5e67c326bb184b712add3042bea6bccc43435bc5
John Howes <john.howes@gmail.com>
2017-05-16 11:37:05 -0500

    Added `grid` Tests

--- src/grid.js
+++ src/grid.js
@@ -5,7 +5,7 @@ import {
 
 var gutterPattern = () => /^(\d*)([a-z]*)$/;
 
-function parseGutter(gutter) {
+function parseValue(gutter) {
   var parsedGutter = gutterPattern().exec(gutter);
   var gutterValue = +parsedGutter[1];
   var gutterUnits = parsedGutter[2];
@@ -16,12 +16,20 @@ function parseGutter(gutter) {
   };
 }
 
-export default function grid(columns, gutter) {
+export default function grid(columns, gutter, margin, test) {
+  margin = margin || "0px";
   return function (element) {
     var wrapper = wrapChildren(element);
-    var parsedGutter = parseGutter(gutter);
+    var parsedGutter = parseValue(gutter);
+    var parsedMargin = parseValue(margin);
 
-    wrapper.style.margin = "-" + (parsedGutter.value / 2) + parsedGutter.units;
+    var calculatedMargin = `calc(-${parsedGutter.value/2}${parsedGutter.units} + ${parsedMargin.value}${parsedMargin.units})`;
+    wrapper.style.margin = calculatedMargin;
+
+    if (test) {
+      wrapper.setAttribute("data-test-margin", calculatedMargin);
+    }
+
     wrapper.style.display = "flex";
     wrapper.style.flexGrow = "1";
     wrapper.style.flexDirection = "row";
@@ -31,20 +39,28 @@ export default function grid(columns, gutter) {
 
     wrapper.setAttribute("data-material-grid-columns", columns);
     wrapper.setAttribute("data-material-grid-gutter", gutter);
+    wrapper.setAttribute("data-material-grid-margin", margin);
   };
 }
 
-grid.column = function column(span, offset) {
+grid.column = function column(span, offset, test) {
   return function (element) {
     var gridWrapper = element.parentElement;
     var columns = +gridWrapper.getAttribute("data-material-grid-columns");
     var gutter = gridWrapper.getAttribute("data-material-grid-gutter");
+    var margin = gridWrapper.getAttribute("data-material-grid-margin")
 
-    var parsedGutter = parseGutter(gutter);
+    var parsedGutter = parseValue(gutter);
+    var parsedMargin = parseValue(margin);
 
     var totalColumns = columns / span;
-    var columnWidth = "calc((100% - " + gutter + " * " + totalColumns + ") / " + totalColumns + ")";
+    var columnWidth = `calc((100% - (${margin} * 2) - (${gutter} * ${totalColumns})) / ${totalColumns})`;
     element.style.width = columnWidth;
+
+    if (test) {
+      element.setAttribute("data-test-column-width", columnWidth);
+    }
+
     var margin = parsedGutter.value / 2 + parsedGutter.units;
     element.style.margin = margin;
 
---
+++ test/grid.js
@@ -0,0 +1,69 @@
+require("./html-document-api");
+const chai = require("chai");
+const should = chai.should();
+import { grid } from "../src";
+import {
+  query
+} from "@lynx-json/jsua-style";
+
+describe("creating a material grid", function () {
+  var element;
+
+  beforeEach(function () {
+    element = document.createElement("div");
+    element.innerHTML = `
+      <div></div>
+    `;
+
+    query(element).each(grid(12, "16px", null, true));
+  });
+
+  it("should create a wrapper around the contents of the element", function () {
+    element.firstElementChild.getAttribute("role").should.equal("presentation");
+  });
+
+  it("should add a half-gutter negative margin to the content wrapper", function () {
+    element.firstElementChild.getAttribute("data-test-margin").should.equal("calc(-8px + 0px)");
+  });
+
+  describe("when creating a column", function () {
+    beforeEach(function () {
+      query(element).each([
+        el => query(el).map(el => el.firstElementChild.children).each(grid.column(4, 0, true))
+      ]);
+    });
+
+    it("should calculate the width based on column span (removing gutter width from calculation)", function () {
+      element.firstElementChild.firstElementChild.getAttribute("data-test-column-width").should.equal("calc((100% - (0px * 2) - (16px * 3)) / 3)");
+    });
+  });
+});
+
+describe("creating a material grid with a margin", function () {
+  var element;
+
+  beforeEach(function () {
+    element = document.createElement("div");
+    element.innerHTML = `
+      <div></div>
+    `;
+
+    query(element).each(grid(12, "16px", "8px", true));
+  });
+
+  it("should subtact half a gutter width from the margin and add it to the to the content wrapper", function () {
+    element.firstElementChild.getAttribute("data-test-margin").should.equal("calc(-8px + 8px)");
+  });
+
+  describe("when creating a column", function () {
+    beforeEach(function () {
+      query(element).each([
+        el => query(el).map(el => el.firstElementChild.children).each(grid.column(4, 0, true))
+      ]);
+    });
+
+    it("should calculate the width based on column span (removing gutter width and grid margin from calculation)", function () {
+      element.firstElementChild.firstElementChild.getAttribute("data-test-column-width").should.equal("calc((100% - (8px * 2) - (16px * 3)) / 3)");
+    });
+  });
+});
