359432b35f83b27f7826b576014a40104308d633
John Howes <john.howes@gmail.com>
2018-05-07 12:34:54 -0500

    Cleanup Dependencies

--- test-browser/iterator-polyfill.js
+++
@@ -1,4 +0,0 @@
-// Note that this is required for Safari.
-if (!HTMLCollection.prototype[Symbol.iterator]) {
-  HTMLCollection.prototype[Symbol.iterator] = Array.prototype[Symbol.iterator];
-}
--- test/html-document-api.js
+++
@@ -1,8 +0,0 @@
-if (typeof window === "undefined") {
-  var jsdom = require("jsdom").jsdom;
-  global.document = jsdom();
-  global.window = document.defaultView;
-  global.Blob = window.Blob;
-  global.FileReader = window.FileReader;
-  global.CustomEvent = window.CustomEvent;
-}
